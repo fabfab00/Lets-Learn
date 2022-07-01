8ed2710dfa10dc57b6915a6990cf37db28437590
fabfab00 <fabiocaruso500@outlook.com>
2022-07-01 10:45:24 +0200

    Creazione classi

---
+++ src/main/java/com/example/Lets_Learn/AssociaLeFrasiTraDiloro.java
@@ -0,0 +1,12 @@
+package com.example.Lets_Learn;
+
+import java.util.ArrayList;
+
+public class AssociaLeFrasiTraDiloro {
+
+    String image;
+    ArrayList<String> list1=new ArrayList<>();
+    ArrayList<String> list2=new ArrayList<>();
+    int correttaList1;
+    int correttaList2;
+}
