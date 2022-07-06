b86233792cad15e6932c881f5608cd5e1897d7a1
martins0801 <86469959+martins0801@users.noreply.github.com>
2022-07-06 10:30:14 +0200

    creazione db arte

--- src/main/java/Classi/DatabaseTeoria.java
+++ src/main/java/Classi/DatabaseTeoria.java
@@ -42,6 +42,7 @@ public class DatabaseTeoria {
     private void creaScienze(){}
 
     private void creaArte(){
+
         ArrayList<Card> colori = new ArrayList<>();
         colori.add(new Card("Arancione","./ImmaginiCard/Arte/Colori/arancione.png"));
         colori.add(new Card("Azzurro","./ImmaginiCard/Arte/Colori/azzurro.png"));
@@ -58,20 +59,58 @@ public class DatabaseTeoria {
         colori.add(new Card("Viola","./ImmaginiCard/Arte/Colori/viola.png"));
 
         ArrayList<Card> materialeScolastico = new ArrayList<>();
-        materialeScolastico.add(new Card("Astuccio","Image/Arte/MaterialeScolastico/astuccio.png"));
-        materialeScolastico.add(new Card("Calcolatrice","Image/Arte/MaterialeScolastico/astuccio.png"));
-        materialeScolastico.add(new Card("Gesso","Image/Arte/MaterialeScolastico/astuccio.png"));
-        materialeScolastico.add(new Card("Gomma","Image/Arte/MaterialeScolastico/astuccio.png"));
-        materialeScolastico.add(new Card("Libro","Image/Arte/MaterialeScolastico/astuccio.png"));
-        materialeScolastico.add(new Card("Matita","Image/Arte/MaterialeScolastico/astuccio.png"));
-        materialeScolastico.add(new Card("Matite Colorate","Image/Arte/MaterialeScolastico/astuccio.png"));
-        materialeScolastico.add(new Card("Penna","Image/Arte/MaterialeScolastico/astuccio.png"));
-        materialeScolastico.add(new Card("Quaderni","Image/Arte/MaterialeScolastico/astuccio.png"));
-        materialeScolastico.add(new Card("Righello","Image/Arte/MaterialeScolastico/astuccio.png"));
-        materialeScolastico.add(new Card("Temperino","Image/Arte/MaterialeScolastico/astuccio.png"));
-        materialeScolastico.add(new Card("Zaino","Image/Arte/MaterialeScolastico/astuccio.png"));
+        materialeScolastico.add(new Card("Astuccio","./ImmaginiCard/Arte/MaterialeScolastico/astuccio.png"));
+        materialeScolastico.add(new Card("Calcolatrice","./ImmaginiCard/Arte/MaterialeScolastico/calcolatrice.png"));
+        materialeScolastico.add(new Card("Gesso","./ImmaginiCard/Arte/MaterialeScolastico/gesso.png"));
+        materialeScolastico.add(new Card("Gomma","./ImmaginiCard/Arte/MaterialeScolastico/gomma.png"));
+        materialeScolastico.add(new Card("Libro","./ImmaginiCard/Arte/MaterialeScolastico/libro.png"));
+        materialeScolastico.add(new Card("Matita","./ImmaginiCard/Arte/MaterialeScolastico/matita.png"));
+        materialeScolastico.add(new Card("Matite Colorate","./ImmaginiCard/Arte/MaterialeScolastico/matiteColorate.png"));
+        materialeScolastico.add(new Card("Penna","./ImmaginiCard/Arte/MaterialeScolastico/penna.png"));
+        materialeScolastico.add(new Card("Quaderni","./ImmaginiCard/Arte/MaterialeScolastico/quaderni.png"));
+        materialeScolastico.add(new Card("Righello","./ImmaginiCard/Arte/MaterialeScolastico/righello.png"));
+        materialeScolastico.add(new Card("Temperino","./ImmaginiCard/Arte/MaterialeScolastico/temperino.png"));
+        materialeScolastico.add(new Card("Zaino","./ImmaginiCard/Arte/MaterialeScolastico/zaino.png"));
+
+        ArrayList<Card> musica = new ArrayList<>();
+        musica.add(new Card("Armonica","./ImmaginiCard/Arte/Musica/armonica.png"));
+        musica.add(new Card("Arpa","./ImmaginiCard/Arte/Musica/arpa.png"));
+        musica.add(new Card("Batteria","./ImmaginiCard/Arte/Musica/batteria.png"));
+        musica.add(new Card("Chitarra classica","./ImmaginiCard/Arte/Musica/chitarraClassica.png"));
+        musica.add(new Card("Chitarra Elettrica","./ImmaginiCard/Arte/Musica/chitarraElettrica.png"));
+        musica.add(new Card("Clarinetto","./ImmaginiCard/Arte/Musica/clarinetto.png"));
+        musica.add(new Card("Fisarmonica","./ImmaginiCard/Arte/Musica/fisarmonica.png"));
+        musica.add(new Card("Flauto","./ImmaginiCard/Arte/Musica/flauto.png"));
+        musica.add(new Card("Lira","./ImmaginiCard/Arte/Musica/lira.png"));
+        musica.add(new Card("Pianoforte","./ImmaginiCard/Arte/Musica/pianoforte.png"));
+        musica.add(new Card("Sassofono","./ImmaginiCard/Arte/Musica/sassofono.png"));
+        musica.add(new Card("Tamburo","./ImmaginiCard/Arte/Musica/tamburo.png"));
+        musica.add(new Card("Triangolo","./ImmaginiCard/Arte/Musica/triangolo.png"));
+        musica.add(new Card("Tromba","./ImmaginiCard/Arte/Musica/tromba.png"));
+        musica.add(new Card("Violino","./ImmaginiCard/Arte/Musica/violino.png"));
+        musica.add(new Card("Xilofono","./ImmaginiCard/Arte/Musica/xilofono.png"));
+
+        ArrayList<Card> vestiti = new ArrayList<>();
+        vestiti.add(new Card("Abito","./ImmaginiCard/Arte/Vestiti/abito.png"));
+        vestiti.add(new Card("Ballerine","./ImmaginiCard/Arte/Vestiti/ballerine.png"));
+        vestiti.add(new Card("Camicia","./ImmaginiCard/Arte/Vestiti/camicia.png"));
+        vestiti.add(new Card("Canotta","./ImmaginiCard/Arte/Vestiti/canotta.png"));
+        vestiti.add(new Card("Cappotto","./ImmaginiCard/Arte/Vestiti/cappotto.png"));
+        vestiti.add(new Card("Felpa","./ImmaginiCard/Arte/Vestiti/felpa.png"));
+        vestiti.add(new Card("Giacca","./ImmaginiCard/Arte/Vestiti/giacca.png"));
+        vestiti.add(new Card("Gonna","./ImmaginiCard/Arte/Vestiti/gonna.png"));
+        vestiti.add(new Card("Impermeabile","./ImmaginiCard/Arte/Vestiti/impermeabile.png"));
+        vestiti.add(new Card("Jeans","./ImmaginiCard/Arte/Vestiti/jeans.png"));
+        vestiti.add(new Card("Maglietta","./ImmaginiCard/Arte/Vestiti/maglietta.png"));
+        vestiti.add(new Card("Maglione","./ImmaginiCard/Arte/Vestiti/maglione.png"));
+        vestiti.add(new Card("Mocassini","./ImmaginiCard/Arte/Vestiti/mocassini.png"));
+        vestiti.add(new Card("Pantaloni","./ImmaginiCard/Arte/Vestiti/pantaloni.png"));
+        vestiti.add(new Card("Scarpe da ginnastica","./ImmaginiCard/Arte/Vestiti/scarpeGinnastica.png"));
+
         arte.add(new Argomento("Colori",colori));
         arte.add(new Argomento("Materiale Scolastico",materialeScolastico));
+        arte.add(new Argomento("Strumenti musicali",musica));
+        arte.add(new Argomento("Vestiti",vestiti));
 
 
 
