c9ddb633a2e97aed6a6c92e1aa289a3b17ebd33e
fabfab00 <fabiocaruso500@outlook.com>
2022-07-07 11:01:24 +0200

    Implementazione esercizi

--- src/main/java/Servlet/ServletEsercizi.java
+++ src/main/java/Servlet/ServletEsercizi.java
@@ -1,9 +1,12 @@
 package Servlet;
 
+import Classi.*;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.ArrayList;
 
 @WebServlet(name = "ServletEsercizi", value = "/ServletEsercizi")
 public class ServletEsercizi extends HttpServlet {
@@ -11,6 +14,75 @@ public class ServletEsercizi extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String materiaString= request.getParameter("materia");
         String argomentoString = request.getParameter("argomento");
+        ArrayList<EserciziPerArgomento> arrayList=new ArrayList<>();
+        DatabaseEsercizi databaseEsercizi=new DatabaseEsercizi();
+        EserciziPerArgomento eserciziPerArgomento;
+        ArrayList<AssociaImmagineConParola> associaImmagineConParola;
+        ArrayList<AssociaLeFrasiTraDiloro> associaLeFrasiTraDiloro;
+        ArrayList<ScriviLaparolaCorretta> scriviLaparolaCorretta;
+        switch (materiaString){
+            case "Italiano" :
+                arrayList=databaseEsercizi.getItaliano();
+                eserciziPerArgomento= databaseEsercizi.getEserciziPerArgomentobyNome(argomentoString, arrayList);
+                associaImmagineConParola=eserciziPerArgomento.getEsercizioAssociaImmagineConParola();
+                associaLeFrasiTraDiloro=eserciziPerArgomento.getEsercizioAssociaLeFrasiTraDiLoro();
+                scriviLaparolaCorretta= eserciziPerArgomento.getEsercizioScriviLaParolaCorretta();
+                request.setAttribute("associaImmagineConParola", associaImmagineConParola.get(0));
+                request.setAttribute("associaLeFrasiTraDiloro", associaLeFrasiTraDiloro.get(0));
+                request.setAttribute("scriviLaparolaCorretta", scriviLaparolaCorretta.get(0));
+                break;
+            case "Storia":
+                arrayList=databaseEsercizi.getStoria();
+                eserciziPerArgomento= databaseEsercizi.getEserciziPerArgomentobyNome(argomentoString, arrayList);
+                associaImmagineConParola=eserciziPerArgomento.getEsercizioAssociaImmagineConParola();
+                associaLeFrasiTraDiloro=eserciziPerArgomento.getEsercizioAssociaLeFrasiTraDiLoro();
+                scriviLaparolaCorretta= eserciziPerArgomento.getEsercizioScriviLaParolaCorretta();
+                request.setAttribute("associaImmagineConParola", associaImmagineConParola.get(0));
+                request.setAttribute("associaLeFrasiTraDiloro", associaLeFrasiTraDiloro.get(0));
+                request.setAttribute("scriviLaparolaCorretta", scriviLaparolaCorretta.get(0));
+                break;
+            case "Geografia" :
+                arrayList=databaseEsercizi.getGeografia();
+                eserciziPerArgomento= databaseEsercizi.getEserciziPerArgomentobyNome(argomentoString, arrayList);
+                associaImmagineConParola=eserciziPerArgomento.getEsercizioAssociaImmagineConParola();
+                associaLeFrasiTraDiloro=eserciziPerArgomento.getEsercizioAssociaLeFrasiTraDiLoro();
+                scriviLaparolaCorretta= eserciziPerArgomento.getEsercizioScriviLaParolaCorretta();
+                request.setAttribute("associaImmagineConParola", associaImmagineConParola.get(0));
+                request.setAttribute("associaLeFrasiTraDiloro", associaLeFrasiTraDiloro.get(0));
+                request.setAttribute("scriviLaparolaCorretta", scriviLaparolaCorretta.get(0));
+                break;
+            case "Matematica" :
+                arrayList=databaseEsercizi.getMatematica();
+                eserciziPerArgomento= databaseEsercizi.getEserciziPerArgomentobyNome(argomentoString, arrayList);
+                associaImmagineConParola=eserciziPerArgomento.getEsercizioAssociaImmagineConParola();
+                associaLeFrasiTraDiloro=eserciziPerArgomento.getEsercizioAssociaLeFrasiTraDiLoro();
+                scriviLaparolaCorretta= eserciziPerArgomento.getEsercizioScriviLaParolaCorretta();
+                request.setAttribute("associaImmagineConParola", associaImmagineConParola.get(0));
+                request.setAttribute("associaLeFrasiTraDiloro", associaLeFrasiTraDiloro.get(0));
+                request.setAttribute("scriviLaparolaCorretta", scriviLaparolaCorretta.get(0));
+                break;
+            case "Arte" :
+                arrayList=databaseEsercizi.getArte();
+                eserciziPerArgomento= databaseEsercizi.getEserciziPerArgomentobyNome(argomentoString, arrayList);
+                associaImmagineConParola=eserciziPerArgomento.getEsercizioAssociaImmagineConParola();
+                associaLeFrasiTraDiloro=eserciziPerArgomento.getEsercizioAssociaLeFrasiTraDiLoro();
+                scriviLaparolaCorretta= eserciziPerArgomento.getEsercizioScriviLaParolaCorretta();
+                request.setAttribute("associaImmagineConParola", associaImmagineConParola.get(0));
+                request.setAttribute("associaLeFrasiTraDiloro", associaLeFrasiTraDiloro.get(0));
+                request.setAttribute("scriviLaparolaCorretta", scriviLaparolaCorretta.get(0));
+                break;
+            case "Scienze" :
+                arrayList=databaseEsercizi.getScienze();
+                eserciziPerArgomento= databaseEsercizi.getEserciziPerArgomentobyNome(argomentoString, arrayList);
+                associaImmagineConParola=eserciziPerArgomento.getEsercizioAssociaImmagineConParola();
+                associaLeFrasiTraDiloro=eserciziPerArgomento.getEsercizioAssociaLeFrasiTraDiLoro();
+                scriviLaparolaCorretta= eserciziPerArgomento.getEsercizioScriviLaParolaCorretta();
+                request.setAttribute("associaImmagineConParola", associaImmagineConParola.get(0));
+                request.setAttribute("associaLeFrasiTraDiloro", associaLeFrasiTraDiloro.get(0));
+                request.setAttribute("scriviLaparolaCorretta", scriviLaparolaCorretta.get(0));
+                break;
+            default: break;
+        }
         RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/General/AssegnaEsercizio.jsp");
         dispatcher.forward(request, response);
     }
